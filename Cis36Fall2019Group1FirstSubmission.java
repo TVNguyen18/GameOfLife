9a1f767593b07dd8d7cc7f61ff86b63bba9c3d45
NguyenVoT22 <53206387+NguyenVoT22@users.noreply.github.com>
2019-09-05 19:47:02 -0700

    Add files via upload

---
+++ Cis36Fall2019Group1FirstSubmission.java
@@ -0,0 +1,12 @@
+/**
+ * Program Name: Cis36Fall2019Group1FirstSubmission.java
+ * Discussion:   Group 1 First Submission
+ * Written By:   Thanhthanh N and Thanhbinh N
+ * Date:         2019/09/24
+ */
+
+public class Cis36Fall2019Group1FirstSubmission {
+    public static void main (String[] args) {
+    
+    }
+}
