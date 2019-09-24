2f1f217985ccd0e33c5acf0f6572410b24cb3ee6
TVNguyen18 <49044385+TVNguyen18@users.noreply.github.com>
2019-09-23 22:55:06 -0700

    Update Cis36Fall2019Group1FirstSubmission.java

--- Cis36Fall2019Group1FirstSubmission.java
+++ Cis36Fall2019Group1FirstSubmission.java
@@ -1,5 +1,5 @@
 /**
- * Program Name: Cis36Fall2019Group1FirstSubmission.java
+ * Program Name: cis36Fall2019Group1FirstSubmission.java
  * Discussion:   Group 1 First Submission
  * Written By:   Thanhthanh N and Thanhbinh N
  * Date:         2019/09/24
@@ -7,6 +7,46 @@
 
 public class Cis36Fall2019Group1FirstSubmission {
     public static void main (String[] args) {
-    
+        System.out.println("Group Members:\n" +
+                "  Thanhthanh Nguyen,\n" +
+                "  Thanhbinh Nguyen\n\n" +
+                "Reading materials:\n" +
+                "  1. Computer Graphics Programming in OpenGL with Java," +
+                "\n     by V. Scott Gordon and John Clevenger\n" +
+                "  2. Tutorials Point JOGL Tutorial,\n" +
+                "     https://www.tutorialspoint.com/jogl/index.htm\n\n" +
+                "Graphic Tools/API Selction:\n" +
+                "  OpenGL\n\n" +
+                "Group Task Division:\n" +
+                "  Thanhthanh: Coding\n" +
+                "  Thanhbinh:  Testing, Researching\n\n" +
+                "  To research: Google tutorials and find examples \n" +
+                "  of other Game of Life projects.");
     }
 }
+
+/* PROGRAM OUTPUT
+Group Members:
+  Thanhthanh Nguyen,
+  Thanhbinh Nguyen
+
+Reading materials:
+  1. Computer Graphics Programming in OpenGL with Java,
+     by V. Scott Gordon and John Clevenger
+  2. Tutorials Point JOGL Tutorial,
+     https://www.tutorialspoint.com/jogl/index.htm
+
+Graphic Tools/API Selction:
+  OpenGL
+
+Group Task Division:
+  Thanhthanh: Coding
+  Thanhbinh:  Testing, Researching
+
+  To research: Google tutorials and find examples 
+  of other Game of Life projects.
+ */
+
+/* Comments on Code_Logic_Output
+No comment.
+ */
